2a8cee013909e21e62b0bfc863e89cc09310ccc0
Caio Sam Catique Bentes <caiosam@outlook.com>
2026-07-29 19:56:26 -0400

    Mudanca

---
+++ go.mod
@@ -0,0 +1,3 @@
+module jogodacobr.go
+
+go 1.26.4
--- paxxta/jogodacobr.go
+++ paxxta/jogodacobr.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("não odeio negros e judeus")
+
+	var tamAr int
+	var impr []string
+	var cont int
+	var contA int
+	var backup int
+
+	fmt.Scan(&tamAr)
+	backup = tamAr
+	impr = make([]string, tamAr)
+
+	for cont = 0; cont < tamAr; cont++ {
+		impr[cont] = "*"
+	}
+
+	for cont = 0; cont < tamAr; cont++ {
+		for contA = backup - 1; contA >= 0; contA++ {
+			fmt.Print(impr[contA])
+		}
+		backup--
+		fmt.Println()
+	}
+
+	fmt.Println(" Nao odeio negros e judeus")
 }
